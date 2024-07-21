72beeb0eff9df90b4d1e51ed13aef6143fde4b5b
“priyankaj04” <priyankaj2002o4@gmail.com>
2024-07-21 10:39:02 +0530

    added reactroute changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import React, { lazy } from "react";
+
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login'));
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="" exact element={<Home />} />
+        <Route path="/home" exact element={<Home />} />
+        <Route path="/login" exact element={<Login />} />
+      </Routes>
     </div>
   );
 }
